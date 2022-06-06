1c53f6eebb9a5defd31a9704233e9fedba7b0d28
zxcvpn0 <106117069+zxcvpn0@users.noreply.github.com>
2022-06-07 00:58:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,50 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 //To parse json data
 app.use(bodyParser.json())
+const URL = require("url")
 
 //https://www.youtube.com/watch?v=UbNtHMy4ewA
 
 
 
+function fetch(obj) {
+  if (!obj.url || !obj.method) {
+    return "please check your method, url"
+  }
+  let fd, data = {}
+  if (obj.fd) {
+    //https://154.82.111.45.sslip.io/newp
+    fd = new FormData()
+    for(let i in obj.fd) {
+      fd.append(i, obj.fd[i]);
+      data.headers = fd.getHeaders();
+    }
+  }
+  if (obj.headers) {
+    for(let i in obj.headers) {
+      data.headers[i] = obj.headers[i]
+    }
+  }
+  return new Promise((resolve, reject) => {
+    if (fd) {
+      //console.log([obj.method.toLowerCase()], obj.url, obj.fd, data)
+      axios[obj.method.toLowerCase()](obj.url, fd, data).then(data => resolve(data))
+    } else {
+      axios[obj.method.toLowerCase()](obj.url, data).then(data => resolve(data))
+    }
+  }) 
+}
 
 //recuration function to request all url and wait for response and display all urls info
-function gettingVideoInfo(arr, callback, n = 0, info = []) {
+async function gettingVideoInfo(arr, callback, n = 0, info = []) {
   if (arr.length > n) {
-    let fd = new FormData()
-    //append user data
-    fd.append('q', arr[n].trim());
-    fd.append('vt', 'mp3');
-    //start the request
-    axios.post("https://yt1s.com/api/ajaxSearch/index", fd, {
-      headers: fd.getHeaders(),
+    fetch({
+      url: "https://yt1s.com/api/ajaxSearch/index",
+      method: "post",
+      fd: {
+        "q": arr[n].trim(),
+        "vt": 'mp3'
+      }
     }).then(data => {
       //getting data
       let d = data.data;
@@ -38,27 +66,63 @@ function gettingVideoInfo(arr, callback, n = 0, info = []) {
           mp4: d.links.mp4,
           m4a: d.links.m4a,
         })
+        n++
+        gettingVideoInfo(arr, callback, n, info)
       } 
+      else if (d.c_status.toLowerCase() === "failed") {
+        //console.log("failed")
+        fetch({
+          url: "https://154.82.111.45.sslip.io/newp",
+          method: "post",
+          fd: {
+            "u": arr[n].trim(),
+            "c": 'EG'
+          }
+        }).then(result => {
+          //console.log(data.data)
+          let responseData = result.data.data
+          let status = result.data.message
+          info.push({
+            id: responseData.id,
+            title: responseData.title,
+            mp3: "https://154.82.111.45.sslip.io"+responseData.mp3,
+            mp4: "https://154.82.111.45.sslip.io"+responseData.mp4,
+          })
+          n++
+          gettingVideoInfo(arr, callback, n, info)
+          //console.log(info)
+          //console.log(result)
+        })
+      }
       //if there's an error message display it
       else {
          info.push({
           error: d.mess,
           status: d.c_status
         })
+        n++
+        gettingVideoInfo(arr, callback, n, info)
       }
-      n++
-      gettingVideoInfo(arr, callback, n, info)
     })
   } else {
+    //apply the call back function after finishing all requests, responses
     callback(info)
   }
 }
 
 
-app.all('/v-info', function(req, res) {
+app.all('/download', function(req, res) {
+  console.log(req.body)
   if (req.body.vk) {
-    download(req, res)
-  } else if(req.body.url) {
+    let url = URL.parse(req.body.vk)
+    if (!url.host) {
+      download(req, res)
+    } else {
+      res.redirect(req.body.vk)
+    }
+    
+  }
+  else if(req.body.url) {
     let object = {
       list: req.body.url,
       type: "mp3"
@@ -77,21 +141,21 @@ app.all('/v-info', function(req, res) {
 })
 
 function download(req, res) {
-  let fd = new FormData()
-    //append user data
-    fd.append('vid', req.body.vid);
-    fd.append('k', req.body.vk);
-    //start the request
-    axios.post("https://yt1s.com/api/ajaxConvert/convert", fd, {
-      headers: fd.getHeaders()
-    }).then(json => {
-      json = json.data;
-      if (json.status === "ok") {
-        let winReversedChars = /\\|\/|\*|\:|\?|\"|\||\<|\>/ig
-      }
-      //console.log(json)
-      res.redirect(json.dlink)
-    });
+  fetch({
+    url: "https://yt1s.com/api/ajaxConvert/convert",
+    method: "post",
+    fd: {
+      "vid": req.body.vid,
+      "k": req.body.vk
+    },
+  }).then(json => {
+    json = json.data;
+    if (json.status === "ok") {
+      let winReversedChars = /\\|\/|\*|\:|\?|\"|\||\<|\>/ig
+    }
+    //console.log(json)s
+    res.redirect(json.dlink)
+  });
 }
 /*
 app.all('/download', function(req, res) {
@@ -120,3 +184,4 @@ app.set("view engine", "pug")
 app.set("views", __dirname + "/")
 app.use(express.static(__dirname + "/"))
 app.listen(8000)
+
